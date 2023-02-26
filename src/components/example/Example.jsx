edfc345cd39f2ffa1767022d944182ff612a99d9
Diman Dimitrov <diman.p.dimitrov@gmail.com>
2023-02-26 14:04:14 +0200

    added trending component

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -6,19 +6,21 @@ import classNames from "classnames";
 import Header from "../header/Header";
 import Avatar from "../avatar/Avatar";
 import Card from "../card/Card";
+import Trending from "../trending/Trending";
 
 export default function Copyright() {
   return (
     <div className={classNames(styles.wrapper)}>
       {/* <Header /> */}
       <Container className={classNames(styles.container)} maxWidth="xl">
-      <Card
+        <Trending/>
+        {/* <Card
           name="Clock"
           mediaUrl="../images/nft.jpg"
           user={{avatar: {url: "",}, verified: true,}}
           price={454545}
           currency="$"
-        />
+        /> */}
         {/* <Paper classNam
         {/* <Paper className={classNames(styles.paper)}>
           <Link href="/about" color="secondary">
---
+++ src/components/trending/Trending.jsx
@@ -0,0 +1,37 @@
+import {
+  Container,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
+import Card from "../card/Card";
+import styles from "./Trending.module.scss";
+
+export default function Trending({ cards = [] }) {
+  return (
+    <Container>
+      <Grid container className={styles.headerContainer}>
+        <Grid item xs={10}>
+          <h1 className="">Trending</h1>
+        </Grid>
+        <Grid item xs={2}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Sort by</InputLabel>
+            <Select>
+              <MenuItem value={10}>This week</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
+          <Grid container spacing={1} className={styles.cardContainer}>
+        {cards.map((card, index) => (
+            <Grid item xs={3} key={index}>
+              <Card {...card} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+}
