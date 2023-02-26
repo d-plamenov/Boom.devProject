26e7da0a47b53f5fe28fb5d20ca62cfb5f558d72
Diman Dimitrov <diman.p.dimitrov@gmail.com>
2023-02-26 14:12:16 +0200

    Added wrapping div

--- src/components/trending/Trending.jsx
+++ src/components/trending/Trending.jsx
@@ -11,27 +11,29 @@ import styles from "./Trending.module.scss";
 
 export default function Trending({ cards = [] }) {
   return (
-    <Container>
-      <Grid container className={styles.headerContainer}>
-        <Grid item xs={10}>
-          <h1 className="">Trending</h1>
+    <div className={styles.trending}>
+      <Container>
+        <Grid container className={styles.headerContainer}>
+          <Grid item xs={10}>
+            <h1 className="">Trending</h1>
+          </Grid>
+          <Grid item xs={2}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Sort by</InputLabel>
+              <Select>
+                <MenuItem value={10}>This week</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
         </Grid>
-        <Grid item xs={2}>
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Sort by</InputLabel>
-            <Select>
-              <MenuItem value={10}>This week</MenuItem>
-            </Select>
-          </FormControl>
-        </Grid>
-      </Grid>
-          <Grid container spacing={1} className={styles.cardContainer}>
-        {cards.map((card, index) => (
+        <Grid container spacing={1} className={styles.cardContainer}>
+          {cards.map((card, index) => (
             <Grid item xs={3} key={index}>
               <Card {...card} />
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </div>
   );
 }
